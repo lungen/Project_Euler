007b8307e4f19064b70ccf376d72c9945eaf628b
lake <lungenfetzer@gmail.com>
2016-12-15 00:43:56 +0100

    solved

--- P25_1000_Fibo_01.py
+++ P25_1000_Fibo_01.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-def fibo(n=22):
+def fibo(n=100000):
     a, b = 0, 1
     for n in range(2, n + 1):
         a, b = b, a+b
-        if len(str(b)) == 3:
+        if len(str(b)) == 1000:
             print(n, b)
             break
 
