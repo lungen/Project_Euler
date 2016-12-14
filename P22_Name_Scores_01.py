4a861c6587fe9f4ad480a9fdabcc41a2b924052f
lungen <lungenfetzer@gmail.com>
2016-12-14 19:39:06 +0000

    SOLVED

--- P22_Name_Scores_01.py
+++ P22_Name_Scores_01.py
@@ -4,17 +4,32 @@ def read_file():
     with open("p022_names.txt", "r") as f:
         data = f.read()
 
-    names = [name for name in data.strip().split(",")]
+    names = [name.lower() for name in data.strip().split(",")]
     return names
+
+
+def abc_value(name):
+    
+    suma = 0
+    for idx, string in enumerate(name):
+        if string != '"':
+            suma += (ord(string) - 96)
+    return suma
+
+
+
+def run_it(names):
+
+    big_suma = 0
+    for idx, name in enumerate(names):
+        big_suma += abc_value(name) * (idx + 1) 
+    print(big_suma)
+
 
 def start():
 
-    names = read_file()
-    print(len(names))
-    print(names[0], names[-1])
-
-    snames = sorted(names)
-    print(snames[0], snames[-1])
-
+    names = sorted(read_file())
+    # abc_value("adam")
+    run_it(names)
 
 start()
