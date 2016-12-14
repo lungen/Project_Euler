b1830fbb98b3310cb0d7559f9da9283894281385
lake <lungenfetzer@gmail.com>
2016-12-15 00:33:39 +0100

    work on

--- P25_1000_Fibo_01.py
+++ P25_1000_Fibo_01.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
-def fibo(n=12):
+def fibo(n=22):
     a, b = 0, 1
-    print("1 ",b)
     for n in range(2, n + 1):
         a, b = b, a+b
-        print(n, b)
+        if len(str(b)) == 3:
+            print(n, b)
+            break
 
 fibo()
