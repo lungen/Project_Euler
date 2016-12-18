841923baf8d4c6e4cc97e493c89658ea41c0db72
lake <lungenfetzer@gmail.com>
2016-12-18 14:37:44 +0100

    working on

--- p19_counting_sundays_02.py
+++ p19_counting_sundays_02.py
@@ -20,22 +20,29 @@ for yourself.
 How many Sundays fell on the first of the month during the twentieth century
 (1 Jan 1901 to 31 Dec 2000)?
 """
-months = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
 # create a year
 years = [1900]
 months = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 d = 1
 first_sundays = []
-for idx, month in enumerate(months):
-    for day in range(1, month + 1):
-        print("{0} {1:>5} {2:>5}".format(idx + 1, day, d))
-        if day == 1 and d == 7:
-            first_sundays.append((idx + 1, day, d))
-            print("")
-        if d == 7:
-            d = 1
-        else:
-            d += 1
+schaltjahr = []
+
+for year in range(1900, 2001):
+    if not year % 4:
+        schaltjahr.append(year)
+
+    for idx, month in enumerate(months):
+        for day in range(1, month + 1):
+            # print("{0} {1:>5} {2:>5} {3:>5}".format(year, idx + 1, day, d))
+            if day == 1 and d == 7:
+                first_sundays.append((year, idx + 1, day, d))
+                print("")
+            if d == 7:
+                d = 1
+            else:
+                d += 1
 
 print(first_sundays)
+for e in schaltjahr:
+    print(e)
