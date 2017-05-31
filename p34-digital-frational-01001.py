387661ae1afda09a47b3b53cc04a645a0e7169c8
lungen <lungenfetzer@gmail.com>
2017-05-31 19:25:23 +0200

    Update p34-digital-frational-01001.py

--- p34-digital-frational-01001.py
+++ p34-digital-frational-01001.py
@@ -1,3 +1,16 @@
+#SOLUTION FOUND IN FORUM:
+def findFact():
+	facts = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+	limit = 362880 * 9
+	for i in xrange(0, limit):
+		total = 0
+		stringI = str(i)
+		for j in stringI:
+			total += facts[int(j)]
+		if (total == i):
+			print i
+
+
 import math
 
 
