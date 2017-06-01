873598679b8b01a36465fd1b21b986773bcf4279
lungen <lungenfetzer@gmail.com>
2017-06-01 12:40:32 +0200

    on work

--- p38-pandigital-multiples-01002.py
+++ p38-pandigital-multiples-01002.py
@@ -21,10 +21,12 @@ concatenated product of an integer with (1,2, ... , n) where n > 1?
 w = []
 limit = 9
 maxi = 0
-lmax = []
+maxa = []
+#lmax = []
 
 #for i in range(1, 987654321 + 1):
-for i in range(13, 30 + 1):
+#for i in range(11, 10001 + 1):
+for i in range(11, 100000000 + 1):
     k = list('123456789')
     #k = list(range(1, 10))
     #print("status k: ", k)
@@ -33,19 +35,28 @@ for i in range(13, 30 + 1):
 
     while j <= limit and not stop:
         res = i * j
+        if len(str(res)) > 9:
+            print("res > 9 - break!", res)
         if '0' in str(res):
-            print("0 in res: ", res)
+            #print("0 in res: ", res)
             break
         for a in str(res):
             if a in k:
                 #print("a in k: ", a, k)
                 k.remove(a)
                 #print("removed a from k: ", a, k)
-        else:
-            if int(res) > maxi:
-                maxi = int(res)
-                lmax.append(res)
-            break
+            else:
+                #print("not in list, break: a: ", a, " k: ", k, " res: ", res)
+                if int(res) > maxi:
+                    maxi = int(res)
+                    #lmax.append(res)
+                stop = True
+                break
 
+        if len(k) == 0 and len(str(res)) == 9 and sorted(str(res)) == '123456789':
+            print("max pan number found: ", res, "i , j", i, j)
+            maxa.append(res)
         j += 1
-print(lmax, maxi)
+
+print(maxi, len(str(maxi)))
+print(len(maxa), sorted(maxa))
