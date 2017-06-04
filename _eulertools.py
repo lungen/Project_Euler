7bdfbb5fe1946f54cca911b7f43a46a20b1b798a
lungen <lungenfetzer@gmail.com>
2017-06-04 22:39:27 +0200

    fin

---
+++ _eulertools.py
@@ -0,0 +1,18 @@
+
+def isPrime(x):
+    n = abs(int(x))
+
+    if n < 2:
+        return True
+    if n == 2:
+        return True
+
+    # check if even number
+    if not n & 1:
+        return False
+
+    for i in range(3, int((n ** 0.5)) + 1, 2):
+        if not n % i:
+            return False
+
+    return True
