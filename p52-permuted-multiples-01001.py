e169bd49a8ebd69502b7eafb300ef76f5082dbc3
lungen <lungenfetzer@gmail.com>
2017-06-13 21:36:27 +0200

    Update p52-permuted-multiples-01001.py

--- p52-permuted-multiples-01001.py
+++ p52-permuted-multiples-01001.py
@@ -8,3 +8,26 @@ contain exactly the same digits, but in a different order.
 Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x,
 and 6x, contain the same digits.
 """
+
+
+i = 2
+j = 0
+k = 2
+stop = False
+
+while i <= 100000000:
+    while not stop and k <= 6:
+        j = i * k
+        if sorted(str(i)) == sorted(str(j)):
+            print("f: ", i, j, k)
+            if k == 6:
+                print("bngo: ", i, sorted("".join(str(i))), k)
+            k += 1
+        else:
+            stop = True
+            k = 2
+
+    i += 1
+    stop = False
+
+print("fin", i)
