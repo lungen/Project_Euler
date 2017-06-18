0d25466feab03a0546e34bbdc7ae7a3013264dcb
lungen <lungenfetzer@gmail.com>
2017-06-18 23:50:02 +0200

    update

--- p51/p51-prime-digit-replacement-02014.py
+++ p51/p51-prime-digit-replacement-02014.py
@@ -72,88 +72,112 @@ def hasImportantMultiples_Start(x):
     sx = ''.join((sorted(str(x))))
     #limp = [00, 11, 22, 33, 44, 55]
     limp = [0, 1, 2, 3, 4, 5]
-    multi = 1
+    multi = 2
     for p in limp:
         if multi * str(p) in sx:
-            print("ipm: ", sx, multi * str(p))
+            print("GO ", sx, multi * str(p))
             return True
 
 
 def hasImportantMultiples_End(x):
-    # for the final eight family search
-    # 199, 211
     sx = ''.join((sorted(str(x))))
-    for p in [33, 44, 55, 66, 77, 88, 99]:
-        if str(p) in sx:
-            print("ipm: ", sx)
+    limp = [3, 4, 5, 6, 7, 8, 9]
+    multi = 2
+    for p in limp:
+        if multi * str(p) in sx:
+            print("GO ", sx, multi * str(p))
             return True
 
-
 # ll = [56003, 56113, 56333, 56443, 56663, 56773, 56993]
-
 #lb = [13, 23, 43, 53, 73, 83]
 
+
 la = []
+lFlat = []
 d = {}
-start = 11
+#start = 11
 #start = 199, 181
-lim = 101
-#start = 56000
-#lim = 60001
+#lim = 501
+start = 56000
+#start = 56009
+lim = 60001
 stop = False
 
-# first number has to have mutliple of 00 - 44 to reach the family
-# condition
+# first number has to have mutliple of 00 - 44 to reach the family condition
 importantPattern = ('01234')
 #importantPattern = ('0123456789')
 
 print(" ------ go ------- ")
 
 # basic iteration, every number is being called only one time
-# 11, 11 + counter, 11 + 2 * counter, 
+# 11, 11 + counter, 11 + 2 * counter
 for nr in range(start, lim):
     if not isPrime(nr):
+        continue
+
+    if nr in lFlat:  # if number already stored
         continue
 
     if not hasImportantMultiples_Start(nr):
         continue
 
-    for counter in range(10, 11, 10):
+    # counter iter: 10, 20, ... 110, 120, ...
+    #for counter in range(10, 151, 10):
+    for counter in range(10, 300, 10):
         nr0 = nr
         nr2 = nr + counter
 
+        # nr + counter iteration: nr + counter, nr + (2 * counter)
         while len(str(nr0)) == len(str(nr2)) and nr2 <= lim:
-            # check if new number is prim
+            # check if family number if len > 2: first number of nr and nr2
+            # has to be the same
+            if len(str(nr2)) > 2:
+                if not hasImportantMultiples_End(nr2):
+                    nr2 += counter
+                    continue
+
+                if not str(nr0)[0] is str(nr2)[0]:
+                    #print("<> ", nr0, nr2)
+                    break
+
             if not isPrime(nr2):
-                break  # go on with next counter
+                nr2 += counter
+                continue  # go on with next number2
 
             lnr = list(str(nr0))
             lnr2 = list(str(nr2))
             ln = []
             ln2 = []
-            print(nr0, nr2)
+            lpet = []
+            la = []
+            #print(":  ", nr0, nr2)
 
             # check for family members
-            # 101, 111, 121, 
+            # 101, 111, 121
             for i, (t, u) in enumerate(zip(lnr, lnr2)):
                 if t == u:
-                    print("f: ", i, t, u)
+                    #print("f: ", i, t, u)
+                    lpet.append((t, i))
                 else:
                     # diff: has to be greater than last one in every case
                     # 00, 11, 22
-                    print('n: ', i, t, u)
-                    # bug: 199 => 229; 1 != 2 => 9 > 2; break + if 
+                    #print('n: ', i, t, u)
+
+                    # bug: 199 => 229; 1 != 2 => 9 > 2; break + if
+                    # 129
+                    # 189
                     if t > u:
-                        print('b! ', u, '<', t)
+                        #print('b! ', u, '<', t)
                         stop = True
                         break
-                    ln.append(t)
-                    ln2.append(u)
+                    else:
+                        ln.append((t, i))
+                        ln2.append((u, i))
 
             if not stop and len(ln) == len(ln2) \
                     and len(set(ln)) == 1 \
                     and len(set(ln2)) == 1:
-                print('bngo: ', nr0, nr2)
+                print('>> ', nr0, nr2)
 
                 if nr2 not in la:
                     la.append(nr2)
@@ -162,22 +186,22 @@ for nr in range(start, lim):
                 elif nr2 not in d[nr0]:
                     d[nr0].append(nr2)
 
-            la = []
-            stop = False
             nr2 += counter
 
         # counter for, show results
-        if len(la) > 0:
-            print(nr0, la)
+        #if len(la) > 0:
+            #print(nr0, la)
 
         # clean
         la = []
         stop = False
         #print("")
 
-print(d)
-#die = {k: len(v) for (k, v) in d.items()}
-#print('\nlen die', len(die), "\n", die)
-#mx = max(die.items(), key=operator.itemgetter(1))[0]
-#print("\n", mx, " - ", die[mx], ": ", d[mx])
+        # flat list
+        lFlat = [item for sublist in d.values() for item in sublist]
 
+print(d)
+die = {k: len(v) for (k, v) in d.items()}
+print('\nlen die', len(die), "\n", die)
+mx = max(die.items(), key=operator.itemgetter(1))[0]
+print("\n", mx, " - ", die[mx], ": ", d[mx])
